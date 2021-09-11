f8eaba3b1b20a14e2783a3d4d05489bba2199828
Christoph Fricke <christoph@frickeonline.de>
2021-09-11 11:56:34 +0200

    refactor(event-bus): use withPubSub HOB for implementation

--- src/actors/event_bus.ts
+++ src/actors/event_bus.ts
@@ -1,19 +1,18 @@
 import { ActorRefFrom, Behavior, EventObject } from "xstate";
-import { WithSubscriptions, createSubscriptions } from "../core/mod";
 import { createBroadcastChannel } from "../utils/broadcast_channel";
 import { getInstanceRandom } from "../utils/identifier";
+import { publish, withPubSub, WithPubSub } from "./pubsub";
 
-type EventBusBehavior<E extends EventObject> = Behavior<
-	WithSubscriptions<E>,
-	null
->;
+type BaseBehavior<E extends EventObject> = Behavior<E, null>;
 
 /** {@link ActorRef} that proxies an received event to all subscribed actors. */
-export type EventBus<E extends EventObject> = ActorRefFrom<EventBusBehavior<E>>;
+export type EventBus<E extends EventObject> = ActorRefFrom<
+	WithPubSub<BaseBehavior<E>>
+>;
 
 interface CreateOptions {
 	/**
-	 * Defines the communication strategy of the created {@link EventBusBehavior}.
+	 * Defines the communication strategy of the created event bus behavior.
 	 * - **direct**: Directly calls the different observers for an event.
 	 * - **global-broadcast**: Extension of **direct** that broadcasts the event as well via a {@link BroadcastChannel}.
 	 * - **broadcast**: Similar to **global-broadcast** but applies additional filtering to stay within a browser tab.
@@ -24,54 +23,51 @@ interface CreateOptions {
 /** Create the {@link Behavior} for an {@link EventBus}, which can be spawned. */
 export function createEventBus<E extends EventObject>(
 	options?: Partial<CreateOptions>
-): EventBusBehavior<E> {
-	if (options?.strategy === "global-broadcast") {
-		return globalBroadcastBusBehavior();
+): WithPubSub<BaseBehavior<E>> {
+	let strategy: BaseBehavior<E>;
+
+	switch (options?.strategy) {
+		case "broadcast":
+			strategy = broadcastBusBehavior(getInstanceRandom());
+			break;
+		case "global-broadcast":
+			strategy = globalBroadcastBusBehavior();
+			break;
+		default:
+			strategy = busBehavior();
+			break;
 	}
 
-	if (options?.strategy === "broadcast") {
-		return broadcastBusBehavior(getInstanceRandom());
-	}
-
-	return busBehavior();
+	return withPubSub(strategy);
 }
 
-function busBehavior<E extends EventObject>(): EventBusBehavior<E> {
-	const subscriptions = createSubscriptions<E>();
-
+function busBehavior<E extends EventObject>(): BaseBehavior<E> {
 	return {
 		initialState: null,
-		transition(state, event) {
-			if (subscriptions.handle(event)) return state;
-
-			subscriptions.publish(event);
+		transition(state, event, ctx) {
+			publish(ctx, event);
 
 			return state;
 		},
 	};
 }
 
-function globalBroadcastBusBehavior<
-	E extends EventObject
->(): EventBusBehavior<E> {
-	const subscriptions = createSubscriptions<E>();
+function globalBroadcastBusBehavior<E extends EventObject>(): BaseBehavior<E> {
 	let channel: BroadcastChannel;
 
 	// TODO: Close the channel once https://github.com/statelyai/xstate/pull/2560 is merged
 
 	return {
 		initialState: null,
-		transition(state, event) {
-			if (subscriptions.handle(event)) return state;
-
-			subscriptions.publish(event);
-			channel.postMessage(event);
+		transition(state, event, ctx) {
+			publish(ctx, event);
+			channel?.postMessage(event);
 
 			return state;
 		},
 		start(ctx) {
 			channel = createBroadcastChannel(ctx.id);
-			channel.onmessage = (msg) => subscriptions.publish(msg.data);
+			channel.onmessage = (msg) => publish(ctx, msg.data);
 			channel.onmessageerror = (msg) =>
 				ctx.observers.forEach((obs) => obs.error(msg));
 
@@ -87,19 +83,16 @@ interface WrappedBroadcastEvent<E extends EventObject> {
 
 function broadcastBusBehavior<E extends EventObject>(
 	contextId: number
-): EventBusBehavior<E> {
-	const subscriptions = createSubscriptions<E>();
+): BaseBehavior<E> {
 	let channel: BroadcastChannel;
 
 	// TODO: Close the channel once https://github.com/statelyai/xstate/pull/2560 is merged
 
 	return {
 		initialState: null,
-		transition(state, event) {
-			if (subscriptions.handle(event)) return state;
-
-			subscriptions.publish(event);
-			channel.postMessage(<WrappedBroadcastEvent<E>>{ contextId, event });
+		transition(state, event, ctx) {
+			publish(ctx, event);
+			channel?.postMessage(<WrappedBroadcastEvent<E>>{ contextId, event });
 
 			return state;
 		},
@@ -107,7 +100,7 @@ function broadcastBusBehavior<E extends EventObject>(
 			channel = createBroadcastChannel(ctx.id);
 			channel.onmessage = (msg) =>
 				msg.data.contextId === contextId
-					? subscriptions.publish(msg.data.event)
+					? publish(ctx, msg.data.event)
 					: void 0;
 			channel.onmessageerror = (msg) =>
 				ctx.observers.forEach((obs) => obs.error(msg));
